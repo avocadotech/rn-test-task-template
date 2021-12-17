053e2826d99f8d42e0f97264b53343e2206ecbdb
Cagyo <cagyodev@gmail.com>
2021-12-17 11:18:19 +0200

    Add sample saga

--- src/actions/app/actions.ts
+++ src/actions/app/actions.ts
@@ -2,4 +2,4 @@ import { createAction } from 'redux-actions';
 
 import { SET_APP_LOADING } from './action-types';
 
-export const setAppLoading = createAction(SET_APP_LOADING);
+export const setAppLoading = createAction<boolean>(SET_APP_LOADING);
